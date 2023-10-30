3efb211c762296a9c833ecccb37b8099f2aafd65
Bruno Voltolini Marchiori <143509108+Bruno04VM@users.noreply.github.com>
2023-10-29 23:11:37 -0300

    Add files via upload

---
+++ config-login.php
@@ -0,0 +1,18 @@
+<?php
+
+$dbHost = '****';
+$dbUsername = '****';
+$dbPassword = '****';
+$dbName = '*****';
+
+$conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+// if ($conexao->connect_errno)
+// {
+//    echo "erro";
+// }
+// else
+// {
+//    echo "conexao bem sucedida!";
+// }
+?>
---
+++ form-pesquisa.php
@@ -0,0 +1,18 @@
+<?php
+
+$dbHost = '*****';
+$dbusername = '*****';
+$dbpassword = '*****';
+$dbName = '*****';
+
+$conexao = new mysqli($dbHost,$dbusername,$dbpassword,$dbName);
+
+ //if($conexao->connect_errno)
+ // {
+ // echo "Erro";
+ // }
+ //else
+ // {
+ //  echo "Conexão efetuada com sucesso";
+ //}
+?>
